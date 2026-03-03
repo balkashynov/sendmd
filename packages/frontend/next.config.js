ad887b569c24c35561f3726602a9d97db1c0ecbc
Ivan <balkashynov+gh@gmail.com>
2026-03-03 02:41:00 -0700

    update backend url

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    const backend = process.env.BACKEND_URL || "http://localhost:3001";
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3001/api/:path*",
+        destination: `${backend}/api/:path*`,
       },
     ];
   },
